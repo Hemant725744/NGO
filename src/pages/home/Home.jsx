e928d5703f21b6fe36f4612c1a8af67595236f2c
Ved Mahajan <mahajanved1234@gmail.com>
2025-06-11 11:46:55 +0530

    deployment changes3

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Intro from "./components/Intro";
-import NPOStatus from "./components/NPOstatus";
+
 import Layout from "./components/layout";
 import Contri from "./components/Contribute";
 import Testimonial from "./components/Testimonial";
@@ -10,13 +10,13 @@ function Home() {
     return (
         <div>
             <Intro />
-            {/* <NPOStatus /> */}
+
             <Layout />
             <Contri />
-            <Testimonial/>
-            <Footer/>
-            <WhatsAppButton/>
-          
+            <Testimonial />
+            <Footer />
+            <WhatsAppButton />
+
 
         </div>
     );
